a0cab889c70fa2a70cee0840f0b64faa8844eebc
Michael V <michaelvrld@gmail.com>
2018-09-22 22:40:49 +0200

    Test latest profile viewers list on front-end

--- js/forum/dist/extension.js
+++ js/forum/dist/extension.js
@@ -1,9 +1,9 @@
 'use strict';
 
-System.register('michaelbelgium/flarum-profile-views/main', ['flarum/app', 'flarum/models/User', 'flarum/components/UserCard', 'flarum/helpers/icon', 'flarum/Model', 'flarum/extend'], function (_export, _context) {
+System.register('michaelbelgium/flarum-profile-views/main', ['flarum/app', 'flarum/models/User', 'flarum/components/UserCard', 'flarum/components/UserPage', 'flarum/components/FieldSet', 'flarum/helpers/icon', 'flarum/Model', 'flarum/utils/ItemList', 'flarum/helpers/avatar', 'flarum/helpers/username', 'flarum/extend'], function (_export, _context) {
     "use strict";
 
-    var app, User, UserCard, icon, Model, extend;
+    var app, User, UserCard, UserPage, FieldSet, icon, Model, ItemList, avatar, username, extend;
     return {
         setters: [function (_flarumApp) {
             app = _flarumApp.default;
@@ -11,10 +11,20 @@ System.register('michaelbelgium/flarum-profile-views/main', ['flarum/app', 'flar
             User = _flarumModelsUser.default;
         }, function (_flarumComponentsUserCard) {
             UserCard = _flarumComponentsUserCard.default;
+        }, function (_flarumComponentsUserPage) {
+            UserPage = _flarumComponentsUserPage.default;
+        }, function (_flarumComponentsFieldSet) {
+            FieldSet = _flarumComponentsFieldSet.default;
         }, function (_flarumHelpersIcon) {
             icon = _flarumHelpersIcon.default;
         }, function (_flarumModel) {
             Model = _flarumModel.default;
+        }, function (_flarumUtilsItemList) {
+            ItemList = _flarumUtilsItemList.default;
+        }, function (_flarumHelpersAvatar) {
+            avatar = _flarumHelpersAvatar.default;
+        }, function (_flarumHelpersUsername) {
+            username = _flarumHelpersUsername.default;
         }, function (_flarumExtend) {
             extend = _flarumExtend.extend;
         }],
@@ -33,6 +43,28 @@ System.register('michaelbelgium/flarum-profile-views/main', ['flarum/app', 'flar
                         ' ',
                         app.translator.trans('flarum_profile_views.forum.user.views_count_text', { viewcount: user.views() })
                     ));
+                });
+
+                extend(UserPage.prototype, 'sidebarItems', function (items) {
+                    var lastViewed = new ItemList();
+                    var testUser = app.store.all('users').filter(function (user) {
+                        return user.id() == 1;
+                    })[0];
+                    console.log(testUser);
+
+                    lastViewed.add('lastUser', m(
+                        'a',
+                        { href: app.forum.attribute('baseUrl') + '/u/' + testUser.id() },
+                        avatar(testUser, { className: 'lastUser-avatar' }),
+                        username(testUser, { className: 'lastUser-name' })
+                    ));
+
+                    items.add('lastViewedUsers', FieldSet.component({
+                        label: 'Last viewed:',
+                        className: 'LastUsers',
+                        children: lastViewed.toArray()
+                    }));
+                    console.log(items);
                 });
             });
         }
--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -1,8 +1,13 @@
 import app from 'flarum/app';
 import User from 'flarum/models/User';
 import UserCard from 'flarum/components/UserCard';
+import UserPage from 'flarum/components/UserPage';
+import FieldSet from 'flarum/components/FieldSet';
 import icon from 'flarum/helpers/icon';
 import Model from 'flarum/Model';
+import ItemList from 'flarum/utils/ItemList';
+import avatar from 'flarum/helpers/avatar';
+import username from 'flarum/helpers/username';
 import { extend } from 'flarum/extend';
 
 app.initializers.add('michaelbelgium-flarum-profile-views', function() {
@@ -18,5 +23,27 @@ app.initializers.add('michaelbelgium-flarum-profile-views', function() {
                 {app.translator.trans('flarum_profile_views.forum.user.views_count_text', {viewcount: user.views()})}
             </span>
         ));
+    });
+
+    extend(UserPage.prototype, 'sidebarItems', function(items) {
+        const lastViewed = new ItemList();
+        const testUser = app.store.all('users').filter(user => user.id() == 1)[0];
+        console.log(testUser);
+
+        lastViewed.add('lastUser', 
+            <a href={app.forum.attribute('baseUrl') + '/u/' + testUser.id()}>
+                {avatar(testUser, {className: 'lastUser-avatar'})}
+                {username(testUser, {className: 'lastUser-name'})}
+            </a>
+        );
+
+        items.add('lastViewedUsers',
+            FieldSet.component({
+              label: 'Last viewed:',
+              className: 'LastUsers',
+              children: lastViewed.toArray()
+            })
+        );
+        console.log(items);
     });
 });

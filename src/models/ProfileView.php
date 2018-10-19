24cc30dba1b3f6de91553ba408430dd8ceb08dcb
Michael V <michaelvrld@gmail.com>
2018-10-19 20:33:07 +0200

    Add relationships in ProfileView model (php)

--- src/models/ProfileView.php
+++ src/models/ProfileView.php
@@ -3,6 +3,7 @@
 namespace michaelbelgium\profileviews\models;
 
 use Flarum\Database\AbstractModel;
+use Flarum\Core\User;
 
 
 class ProfileView extends AbstractModel
@@ -10,4 +11,14 @@ class ProfileView extends AbstractModel
 	protected $table = "users_profile_views";
 
 	protected $fillable = ["ip", "viewer_id", "viewed_id"];
+
+	public function viewer()
+	{
+		return $this->hasOne(User::class, 'id', 'viewer_id');
+	}
+
+	public function viewedUser()
+	{
+		return $this->hasOne(User::class, 'id', 'viewed_id');
+	}
 }

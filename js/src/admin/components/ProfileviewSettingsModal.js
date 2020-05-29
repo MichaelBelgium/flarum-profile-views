29c1dc28bd5d9f5ce862b081fc61977e84f5ef04
Michael V <michaelvrld@gmail.com>
2020-05-29 20:47:19 +0200

    Add setting to track guests

--- js/src/admin/components/ProfileviewSettingsModal.js
+++ js/src/admin/components/ProfileviewSettingsModal.js
@@ -12,6 +12,13 @@ export default class ProfileviewSettingsModal extends SettingsModal {
 
     form() {
         return [
+            <div className="Form-group">
+                {Switch.component({
+                    state: this.setting('michaelbelgium-profileviews.track_guests')() == 1,
+                    onchange: this.setting('michaelbelgium-profileviews.track_guests'),
+                    children: app.translator.trans('michaelbelgium-flarum-profile-views.admin.settings.track_guests_label')
+                })}
+            </div>
         ];
     }
 }
--- src/Controllers/CreateUserProfileViewController.php
+++ src/Controllers/CreateUserProfileViewController.php
@@ -8,14 +8,25 @@ use Psr\Http\Server\RequestHandlerInterface;
 use Zend\Diactoros\Response\JsonResponse;
 use Flarum\User\User;
 use Carbon\Carbon;
+use Flarum\Settings\SettingsRepositoryInterface;
 use Michaelbelgium\Profileviews\Models\UserProfileView;
 
 class CreateUserProfileViewController implements RequestHandlerInterface
 {
+    private $settings;
+
+    public function __construct(SettingsRepositoryInterface $settings) {
+        $this->settings = $settings;
+    }
+
     public function handle(Request $request): Response
     {
         $userId = array_get($request->getParsedBody(), 'viewedUser');
         $visitor = array_get($request->getParsedBody(), 'viewer');
+
+        if(!$this->settings->get('michaelbelgium-profileviews.track_guests', false) && $visitor === null) {
+            return new JsonResponse(['error' => 'Tracking guests not been set']);
+        }
 
         if($userId == $visitor) return new JsonResponse(['error' => 'Visitor is the same as viewed user']);
 

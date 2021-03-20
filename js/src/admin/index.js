66ffaca9dc1d2294ddf80470b4005acd52a72869
Michael V <michaelvrld@gmail.com>
2021-03-20 13:12:39 +0100

    Use registerSetting in stead of settingsmodal

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,6 +1,14 @@
 import { extend } from 'flarum/extend';
-import ProfileviewSettingsModal from './components/ProfileviewSettingsModal';
 
 app.initializers.add('michaelbelgium-admin-profile-views', function() {
-    app.extensionSettings['michaelbelgium-profile-views'] = () => app.modal.show(ProfileviewSettingsModal);
+    app.extensionData.for('michaelbelgium-profile-views')
+        .registerSetting({
+            setting: 'michaelbelgium-profileviews.track_guests',
+            label: app.translator.trans('michaelbelgium-flarum-profile-views.admin.settings.track_guests_label'),
+            type: 'boolean'
+        }).registerSetting({
+            setting: 'michaelbelgium-profileviews.max_listcount',
+            label: app.translator.trans('michaelbelgium-flarum-profile-views.admin.settings.max_viewcount_label'),
+            type: 'number'
+        });
 });
